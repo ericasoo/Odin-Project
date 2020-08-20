2568812897eb8becc0c032a41bdf36777d14a69c
Erica Soo <erica.jasmine.soo@gmail.com>
2020-08-20 12:58:46 +1000

    Update etch.js

--- etch-a-sketch/etch.js
+++ etch-a-sketch/etch.js
@@ -3,8 +3,14 @@ const container = document.querySelector('#container');
 let size = 525;
 let cells = 16;
 
+const buttons = document.querySelectorAll('button');
+
+let mode = 1;
 
 function createGrid(cells) {
+
+  container.textContent = "";
+  
   let cellSize = size/cells;
 
   for (let i = 0; i < cells; i++) {
@@ -21,10 +27,10 @@ function createGrid(cells) {
           cell.style.width = `${cellSize}px`;
           cell.style.height = `${cellSize}px`;
           cell.style.backgroundColor = 'lightgrey';
+          cell.setAttribute("counter", 10);
 
-          cell.onmouseover = function() {
-            cell.style.backgroundColor = 'black';
-          }
+          fillGrid(cell);
+
           row.appendChild(cell);
           container.appendChild(row);
 
@@ -33,12 +39,35 @@ function createGrid(cells) {
       }
 };
 
+function fillGrid(cell) {
+  cell.onmouseover = function() {
+    if (mode === 1) {
+      cell.style.backgroundColor = 'black';
+    } else if (mode === 2) {
+      cell.style.backgroundColor = `rgb(${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)},${Math.floor(Math.random()*256)})`;
+    } else if (mode === 3) {
+      let counter = cell.getAttribute("counter");
+      counter -= 1;
+      cell.style.filter= `brightness(${counter*10}%)`;
+      cell.setAttribute("counter",counter);
+    }
+  }
+}
+
 createGrid(cells);
 
 
+function handleClick(event) {
+  let e = event.currentTarget;
+  if (e.id === 'reset') {
+    mode = 1;
+    createGrid(cells);
+  }
+  else if (e.id === 'normal') {mode = 1}
+  else if (e.id === 'colourful') {mode = 2}
+  else if (e.id === 'gradual') {mode = 3}
+}
 
-
-const reset = document.querySelector('#reset');
-const normal = document.querySelector('#normal');
-const colourful = document.querySelector('#colourful');
-const gradual = document.querySelector('#gradual');
+buttons.forEach((button) => {
+  button.addEventListener('click', handleClick)
+});
